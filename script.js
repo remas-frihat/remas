aa13b614fe09ba4dd628eb91fb47c4822d69a4b1
remas-frihat <remasr20082008@gmail.com>
2026-05-12 12:35:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+// let a=15;
+// let b="remas";
+// let d="true";//or false
+// console.log(typeof(a));
+// const e=10;
+
+// console.log(b.length);
+
+
+
+// if(a>5){
+
+//     alert("a large than 5");
+
+// }
+//let a=prompt("enter your name");
+//a = Number(a);
+//alert(typeof(a));
+
+//for(let i=0; i<5; i++){
+  //  alert(i);
+//}
+
+//let num=4;
+
+//while(num>3)[
+//    alert(num)
+//]
+
+//let num=3;
+
+//do{
+ //   console.log(num);
+//}
+ //   while (num>3);
+
+//  let ar=[1,5,'remas'];
+//    alert(typeof(ar));
+
+//document.querySelector('h1').addEventListener('click',function(){
+    //this.style.color='red';
+//    document.querySelector('p').style.color='red'
+
+
+//})
+
+//document.addEventListener('keydown',function(event){
+
+//console.log(event);
+//if(event.key==='a'){
+// document.querySelector('h1').style.fontSize='40px';  
+
+//}
+//})  
+
+ //function run(id,b){
+//    alert(id);
+// }
+
+// run(10,20); 
+function Student(id , name){
+    
+
+    this.name=name;
+    this.id=id;
+
+}
+
+let st1=new Student(2852026,'remas');
+alert(st1.id);
+alert(st2.name);
+
+
+
+
+
+
+
+// switch (a){
+//     case 5:
+//             alert("it is 5");
+
+//             alert("it is a");
+//             break;
+
+
+//             default:
+//             alert("undifine");
+// }
